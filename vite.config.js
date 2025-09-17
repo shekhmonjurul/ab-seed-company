92d999540d111e2cd6c9759d518427c260f3fc23
shekhmonjurul <shekhmonjurul@gmail.com>
2025-09-17 17:11:45 +0600

    add bass

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',       // production build directory
-    sourcemap: false,     // source map production এ সাধারণত off রাখি
-    minify: 'esbuild',    // minification (default)
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // chunking strategy
-      },
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  base: "/"
 })
