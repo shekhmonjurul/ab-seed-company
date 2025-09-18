47f3652e0bed4c310e16b43a3849c82ddafe4479
shekhmonjurul <shekhmonjurul@gmail.com>
2025-09-18 18:14:34 +0600

    a to link

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Button({ children, href }) {
-    return   <a href={href}>{children}</a>
+    return   <Link to={href}>{children}</Link>
 }
