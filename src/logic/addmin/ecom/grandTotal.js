75aa9942e7f4fe82f29598b1c31119943a70ae36
shekhmonjurul <shekhmonjurul@gmail.com>
2025-10-15 17:59:19 +0600

    ok

--- src/logic/addmin/ecom/grandTotal.js
+++
@@ -1,8 +0,0 @@
-export default function grandtotal(formData){
-     const grandtotal =
-    formData.subtotal +
-    formData.deliverycharge -
-    formData.discount -
-    formData.advance;
-    return grandtotal
-}
