a950f536bb7752126285824a7029b937d7db7592
shekhmonjurul <shekhmonjurul@gmail.com>
2025-10-16 14:46:53 +0600

    open

--- src/component/Addmin/e-com/order/DelivaryCard.jsx
+++ src/component/Addmin/e-com/order/DelivaryCard.jsx
@@ -1,19 +1,26 @@
-export default function 
-() {
+export default function DelivaryCard ({currierInfo={
+    name: "Currier name",
+    successRateing: 0,
+    toatl: 0,
+    success: 0,
+    cancelled: 0,
+    parchen: 45
+
+}}) {
     return (
         <div className="w-60 border border-gray-200 rounded-2xl shadow-md  font-sans text-black text-left">
 
-            <h2 className="text-lg font-semibold mb-2 p-4">Pathao</h2>
+            <h2 className="text-lg font-semibold mb-2 p-4">{currierInfo?.name}</h2>
             <hr className="w-full text-gray-200" />
             <div className="px-4">
-                <p className="text-green-600 text-sm font-medium">Success Rate: 100%</p>
-                <p className="text-sm">Total: 2</p>
-                <p className="text-sm">Success: 2</p>
-                <p className="text-sm  mb-2">Cancelled: 0</p>
+                <p className="text-green-600 text-sm font-medium">Success Rate: {currierInfo?.successRateing} %</p>
+                <p className="text-sm">Total: {currierInfo?.toatl}</p>
+                <p className="text-sm">Success: {currierInfo?.success}</p>
+                <p className="text-sm  mb-2">Cancelled: {currierInfo?.cancelled}</p>
             </div>
             <hr className="w-full text-gray-200" />
             <div className="w-[85%] h-2 bg-gray-200 rounded my-2 mx-4">
-                <div className="h-full bg-green-500 rounded  w-[75%]"></div>
+                <div className={`h-full bg-green-500 rounded  w-[${currierInfo?.parchen}%]`}></div>
             </div>
         </div>
     )
--- src/pages/Addmin/e-com/order/OpenOrder.jsx
+++ src/pages/Addmin/e-com/order/OpenOrder.jsx
@@ -9,6 +9,7 @@ import useFetch from "../../../../hooks/useFetch"
 export default function OpenOrder() {
     const { orderID } = useSearchParams()
     const { data, error } = useFetch(`http://localhost:5000/api/weborders/${orderID}`, { method: "get" }, [])
+    // const { currier, currierError } = useFetch(`http://localhost:5000/api/fraudchecker`, { method: "get", body: JSON.stringify(data?.order?.billing?.phone) })
 
     const usrdata = {
         name: data?.order?.billing?.first_name || "",
@@ -21,10 +22,6 @@ export default function OpenOrder() {
         <div>
             <OrderNav />
             <div className="mt-4 bg-white p-4 flex gap-4 justify-center items-center">
-                <DeliveryCard />
-                <DeliveryCard />
-                <DeliveryCard />
-                <DeliveryCard />
                 <DeliveryCard />
             </div>
             <div className="w-full flex justify-center items-center bg-white">
